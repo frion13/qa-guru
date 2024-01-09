206fd9676170318d4f35d41c539546145b5edb92
frion13 <frio_13@bk.ru>
2024-01-09 17:04:25 +0300

    fix

--- src/test/java/testGithubSelenide/Homework.java
+++ src/test/java/testGithubSelenide/Homework.java
@@ -3,22 +3,34 @@ package testGithubSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Homework {
     @Test
     void testSoftAssertions(){
-        open("https://github.com/selenide/selenide");
+        open("https://github.com/selenide/selenide/");
         $("#wiki-tab").click();
-        $(".markdown-body").shouldHave(text("Soft assertions"));
-        sleep(5000);
+        $("button.Link--muted.js-wiki-more-pages-link").click();
+        $(" .wiki-pages-box").shouldHave(text("SoftAssertions")).shouldBe(visible);
 
     }
 
     @Test
     void testJunitExampleCode(){
         open("https://github.com/selenide/selenide/wiki/SoftAssertions");
-        $("body").shouldHave(text("Using JUnit5 extend test class:"));
+        $("body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
     }
 
 }
