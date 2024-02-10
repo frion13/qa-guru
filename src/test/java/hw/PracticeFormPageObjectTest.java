11413809e19002bf9ef855b105ba671f27118d2c
John Doe <frio_13@bk.ru>
2024-02-10 23:34:37 +0300

    hw jenkins

--- src/test/java/hw/PracticeFormPageObjectTest.java
+++ src/test/java/hw/PracticeFormPageObjectTest.java
@@ -1,13 +1,23 @@
 package hw;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 import static utils.RandomUtils.*;
 
+@Tag("registraion")
 public class PracticeFormPageObjectTest extends TestBase {
+
     RegistrationPage registrationPage = new RegistrationPage();
     Faker faker = new Faker();
     String firstName = faker.name().firstName(),
@@ -38,60 +48,104 @@ public class PracticeFormPageObjectTest extends TestBase {
             stateAndCity = String.format("%s %s", state, city);
 
 
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+
     @BeforeEach
     void openPage() {
-        registrationPage.openPage();
+        step("Открыть форму", () -> {
+            registrationPage.openPage();
+        });
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
 
     }
 
     @Test
     void practiceFormTest() {
-        registrationPage.setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(phoneNumber)
-                .setDateOfBirth(month, year)
-                .setSubject(subject)
-                .setHobbies(hobbies)
-                .uploadPicture(picture)
-                .setCurrentAdrress(currentAddress)
-                .setStateAndCity(state, city)
-                .submit()
-                .checkResult(checkName, name)
-                .checkResult(checkEmail, userEmail)
-                .checkResult(checkGender, gender)
-                .checkResult(checkMobile, phoneNumber)
-                .checkResult(checkDateOfBirth, birthDay)
-                .checkResult(checkSubjects, subject)
-                .checkResult(checkHobbies, hobbies)
-                .checkResult(checkPicture, picture)
-                .checkResult(checkAdress, currentAddress)
-                .checkResult(checkStateAndCity, stateAndCity);
+        step("Заполнить форму", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(userEmail)
+                    .setGender(gender)
+                    .setUserNumber(phoneNumber)
+                    .setDateOfBirth(month, year)
+                    .setSubject(subject)
+                    .setHobbies(hobbies)
+                    .uploadPicture(picture)
+                    .setCurrentAdrress(currentAddress)
+                    .setStateAndCity(state, city)
+                    .submit();
+        });
+
+        step("Проверка результатов", () -> {
+            registrationPage.checkResult(checkName, name)
+                    .checkResult(checkEmail, userEmail)
+                    .checkResult(checkGender, gender)
+                    .checkResult(checkMobile, phoneNumber)
+                    .checkResult(checkDateOfBirth, birthDay)
+                    .checkResult(checkSubjects, subject)
+                    .checkResult(checkHobbies, hobbies)
+                    .checkResult(checkPicture, picture)
+                    .checkResult(checkAdress, currentAddress)
+                    .checkResult(checkStateAndCity, stateAndCity);
+        });
 
     }
 
     @Test
     void formWithMinData() {
-        registrationPage.setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(phoneNumber)
-                .submit()
-                .checkResult(checkName, name)
-                .checkResult(checkEmail, userEmail)
-                .checkResult(checkGender, gender)
-                .checkResult(checkMobile, phoneNumber);
+        step("Заполнить форму", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(userEmail)
+                    .setGender(gender)
+                    .setUserNumber(phoneNumber)
+                    .submit();
+        });
+        step("Проверка результатов", () -> {
+            registrationPage.checkResult(checkName, name)
+                    .checkResult(checkEmail, userEmail)
+                    .checkResult(checkGender, gender)
+                    .checkResult(checkMobile, phoneNumber);
+        });
     }
 
     @Test
     void negativeTest() {
-        registrationPage.setGender(gender)
-                .setUserNumber(phoneNumber)
-                .submit()
-                .checkBorderColor();
-
+        step("Заполнить форму", () -> {
+            registrationPage.setGender(gender)
+                    .setUserNumber(phoneNumber)
+                    .submit();
+        });
+        step("Проверка результатов", () -> {
+            registrationPage.checkBorderColor();
+        });
 
     }
 
