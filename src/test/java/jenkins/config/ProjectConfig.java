b146d978495611265c4425ea2b8e8ce8880d9150
John Doe <frio_13@bk.ru>
2024-02-17 23:38:26 +0300

    param

---
+++ src/test/java/jenkins/config/ProjectConfig.java
@@ -0,0 +1,19 @@
+package jenkins.config;
+
+import org.aeonbits.owner.Config;
+
+
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/${environment}.properties"})
+public interface ProjectConfig extends Config {
+    @Key("web.url")
+    String webUrl();
+    @Key("api.url")
+    String apiUrl();
+    @Key("api.port")
+    String apiPort();
+
+}
--- src/test/java/jenkins/properties/SystemPropertiesTests.java
+++ src/test/java/jenkins/properties/SystemPropertiesTests.java
@@ -2,8 +2,11 @@ package jenkins.properties;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import testgithubselenide.SelenideReposytorySearch;
 
 import java.lang.module.Configuration;
+
+import static java.lang.String.format;
 
 public class SystemPropertiesTests {
     @Test
@@ -51,10 +54,21 @@ public class SystemPropertiesTests {
         String browserVersion = System.getProperty("browser_version", "100.0");
         String browserSize = System.getProperty("browser_size", "1920x1080");
 
-        String browserConfig = String.format("Browser: %s, version: %s, window size: %s",
+        String browserConfig = format("Browser: %s, version: %s, window size: %s",
                 browser, browserVersion, browserSize);
 
-        //gradle property_test -Dbrowser=mozilla -Dbrowser_version=96.0 -Dbrowser_size=1080x720
+        //gradle property1_test -Dbrowser=mozilla -Dbrowser_version=96.0 -Dbrowser_size=1080x720
         System.out.println(browserConfig);
+    }
+
+
+    @Test
+    @Tag("property3")
+    void systemProperty7Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s, hoe are you?", name);
+        //gradle property3_test -Dname="Alex Egorov"
+        System.out.println(message);
+
     }
 }
