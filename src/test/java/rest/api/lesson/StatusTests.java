d1676f5b5cb03cf93fd8b37e4016218747f26680
John Doe <frio_13@bk.ru>
2024-03-12 00:05:09 +0300

    rest assured

---
+++ src/test/java/rest/api/lesson/StatusTests.java
@@ -0,0 +1,121 @@
+package rest.api.lesson;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StatusTests {
+     /*
+    1. Make request to https://selenoid.autotests.cloud/status
+    2. Get response { total: 20, used: 0, queued: 0, pending: 0, browsers: ...
+    3. Check total is 20
+     */
+
+    @Test
+    void CheckTotalMini() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void CheckTotalWithLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(20));
+    }
+
+    @Test
+    void CheckTotalWithSomeLogs() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .body("total", is(20));
+    }
+
+    @Test
+    void CheckTotalWithStatusCode() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+
+    @Test
+    void CheckTotalWithChrome() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalWithJsonSchema() {
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath(("schemas/status-response-schema.json")))
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalWithAssert() {
+        Response statusResponse = given()
+                .log().uri()
+                .log().method()
+//                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/status-response-schema.json"))
+                .extract().response();
+
+//        assertEquals(20, (int) statusResponse.path("total"));
+        assertThat(statusResponse.path("total"), is(20));
+        assertThat(statusResponse.path("browsers.chrome"), hasKey("100.0"));
+        //сделать assertJ
+
+    }
+
+}
